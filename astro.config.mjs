513506a5fc4cc6258592a37af1b71613fae94bfa
Ruben Mate <79601967+rubenmate@users.noreply.github.com>
2022-08-18 02:18:44 +0200

    feat: add blog post view with markdown styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,20 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+    integrations: [tailwind(), react()],
+    markdown: {
+        shikiConfig: {
+            // Choose from Shiki's built-in themes (or add your own)
+            // https://github.com/shikijs/shiki/blob/main/docs/themes.md
+            theme: "dracula",
+            // Add custom languages
+            // Note: Shiki has countless langs built-in, including .astro!
+            // https://github.com/shikijs/shiki/blob/main/docs/languages.md
+            langs: [],
+        },
+    },
 });
