dddcce4128dff89d2a384d7e2789ceb253b9a417
Ruben Mate <79601967+rubenmate@users.noreply.github.com>
2022-08-23 01:06:23 +0200

    feat: add rss feed

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
     integrations: [tailwind(), react()],
+    site: "https://rubenmate.com",
     markdown: {
         shikiConfig: {
             // Choose from Shiki's built-in themes (or add your own)
